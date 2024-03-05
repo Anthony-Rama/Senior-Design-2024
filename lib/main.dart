6b0b3aa34493d38ad23f8a498c2c8d28b496fdd1
dgarcia06 <dgarcia3@pride.hofstra.edu>
2024-03-05 12:39:45 -0500

    Fixed main. App launches, firebase sign up not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,17 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mobileapp/Screens/login.dart';
 import 'package:mobileapp/SocialMedia/feed.dart';
 import 'package:mobileapp/Screens/signup.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-/*void main() {
-  runApp(const MyApp());
-}*/
-
-/*void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-}*/
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // initialise app based on platform- web or mobile
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyBf5wIfnaabJW3Jqw9bJjVEbwXCSNNIOaw", 
-        appId: "1:1062001949517:web:96aad9bcaeffc8b6375504",
-        messagingSenderId: "1062001949517",
-        projectId: "bellboard-2024",
-        storageBucket: 'bellboard-2024.appspot.com'
-      ),
-    );
-  } else {
-    await Firebase.initializeApp();
-  }
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print('Firebase initialized successfully');
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
