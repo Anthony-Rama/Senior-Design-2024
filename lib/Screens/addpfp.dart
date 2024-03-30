0458ad2604f8beb8fb149b02c2928cfaa56a9916
Anthony Rama <anthonyhrama@gmail.com>
2024-03-30 16:58:01 -0400

    Updated PFP upload

--- lib/Screens/addpfp.dart
+++ lib/Screens/addpfp.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:mobileapp/SocialMedia/feed.dart'; // Make sure this import is correct
+import 'package:mobileapp/Screens/verifyemail.dart';
 
 class AddPFPScreen extends StatefulWidget {
-  const AddPFPScreen({super.key});
+  const AddPFPScreen({Key? key}) : super(key: key);
 
   @override
   _AddPFPScreenState createState() => _AddPFPScreenState();
@@ -17,14 +17,6 @@ class _AddPFPScreenState extends State<AddPFPScreen> {
   File? _image;
 
   Future<void> uploadImage() async {
-    if (_image == null) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('No image selected.'),
-        backgroundColor: Colors.red,
-      ));
-      return;
-    }
-
     try {
       final Reference storageRef = FirebaseStorage.instance
           .ref()
@@ -37,14 +29,13 @@ class _AddPFPScreenState extends State<AddPFPScreen> {
           .doc(FirebaseAuth.instance.currentUser!.uid)
           .update({'profilePic': imageURL});
 
-      // Navigate to Feed Screen
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const FeedScreen()),
+        MaterialPageRoute(builder: (context) => const VerifyEmailScreen()),
       );
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Failed to upload image. Please try again. $e'),
+        content: Text('Failed to upload image. Please try again.'),
         backgroundColor: Colors.red,
       ));
     }
@@ -61,11 +52,17 @@ class _AddPFPScreenState extends State<AddPFPScreen> {
   }
 
   Future<void> skipImage() async {
-    // No need to update if skipping
-    // Navigate directly to Feed Screen
+    const String defaultImageURL =
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png';
+
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .update({'profilePic': defaultImageURL});
+
     Navigator.pushReplacement(
       context,
-      MaterialPageRoute(builder: (context) => const FeedScreen()),
+      MaterialPageRoute(builder: (context) => const VerifyEmailScreen()),
     );
   }
 
@@ -87,15 +84,25 @@ class _AddPFPScreenState extends State<AddPFPScreen> {
                 ? CircleAvatar(
                     radius: 70,
                     backgroundImage: FileImage(_image!),
+                    child: IconButton(
+                      icon: Icon(Icons.edit, color: Colors.white),
+                      onPressed: selectImage,
+                    ),
                   )
-                : const CircleAvatar(
+                : CircleAvatar(
                     radius: 70,
                     backgroundImage: NetworkImage(
-                        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png'),
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+                    ),
+                    child: IconButton(
+                      icon:
+                          Icon(Icons.add_photo_alternate, color: Colors.white),
+                      onPressed: selectImage,
+                    ),
                   ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: uploadImage,
+              onPressed: _image != null ? uploadImage : null,
               style: ElevatedButton.styleFrom(backgroundColor: Colors.red[400]),
               child: const Text('Upload & Continue',
                   style: TextStyle(color: Colors.white)),
@@ -105,7 +112,7 @@ class _AddPFPScreenState extends State<AddPFPScreen> {
               onPressed: skipImage,
               style: ElevatedButton.styleFrom(backgroundColor: Colors.red[400]),
               child: const Text('Skip', style: TextStyle(color: Colors.white)),
-            )
+            ),
           ],
         ),
       ),
