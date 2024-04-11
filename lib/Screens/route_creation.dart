50dd28b90ce8ade44409847c3d05db6e3aebee62
dgarcia06 <dgarcia3@pride.hofstra.edu>
2024-04-11 18:05:47 -0400

    Flattened offset array to an array that the Pi code can read

--- lib/Screens/route_creation.dart
+++ lib/Screens/route_creation.dart
@@ -6,18 +6,18 @@ class CustomRouteGridScreen extends StatefulWidget {
 }
 
 class _CustomRouteGridScreenState extends State<CustomRouteGridScreen> {
-  List<List<bool>> gridState = List.generate(16, (_) => List.filled(8, false));
+  List<List<bool>> gridState = List.generate(9, (_) => List.filled(5, false));
 
   // holds coordinates on the climbing board image relative to the image size
   List<List<Offset>> holdCoordinates =
-      List.generate(16, (_) => List.filled(8, Offset.zero));
+      List.generate(9, (_) => List.filled(5, Offset.zero));
 
   @override
   void initState() {
     super.initState();
-    holdCoordinates = List.generate(16, (i) {
-      return List.generate(8, (j) {
-        return Offset(20 + j * 60, 20 + i * 40); // values can be adjuste here
+    holdCoordinates = List.generate(9, (i) {
+      return List.generate(5, (j) {
+        return Offset(20 + j * 60, 20 + i * 40); // values can be adjusted here
       });
     });
   }
@@ -40,8 +40,8 @@ class _CustomRouteGridScreenState extends State<CustomRouteGridScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
-                width: size.width * 2.0, // image width
-                height: size.width * 2.0, // image height
+                width: size.width * 1.8, // image width
+                height: size.width * 1.8, // image height
                 child: Stack(
                   children: [
                     Image.asset(
@@ -49,34 +49,37 @@ class _CustomRouteGridScreenState extends State<CustomRouteGridScreen> {
                       fit: BoxFit.cover,
                     ),
                     Positioned.fill(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: List.generate(
-                          gridState.length,
-                          (rowIndex) => Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: List.generate(
-                              gridState[rowIndex].length,
-                              (colIndex) => GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    gridState[rowIndex][colIndex] =
-                                        !gridState[rowIndex][colIndex];
-                                  });
-                                },
-                                child: Container(
-                                  // adjust hold size
-                                  width: size.width * 0.09,
-                                  height: size.width * 0.09,
-                                  margin: const EdgeInsets.symmetric(
-                                      // adjust space between rows and columns
-                                      horizontal: 7.2,
-                                      vertical: 3),
-                                  decoration: BoxDecoration(
-                                    color: gridState[rowIndex][colIndex]
-                                        ? Colors.blue
-                                        : Colors.transparent,
-                                    border: Border.all(color: Colors.black),
+                      child: Transform.translate(
+                        offset: Offset(3.0, 28.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: List.generate(
+                            gridState.length,
+                            (rowIndex) => Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: List.generate(
+                                gridState[rowIndex].length,
+                                (colIndex) => GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      gridState[rowIndex][colIndex] =
+                                          !gridState[rowIndex][colIndex];
+                                    });
+                                  },
+                                  child: Container(
+                                    // adjust hold size
+                                    width: size.width * 0.09,
+                                    height: size.width * 0.09,
+                                    margin: const EdgeInsets.symmetric(
+                                        // adjust space between rows and columns
+                                        horizontal: 22.6,
+                                        vertical: 11.5),
+                                    decoration: BoxDecoration(
+                                      color: gridState[rowIndex][colIndex]
+                                          ? Colors.blue
+                                          : Colors.transparent,
+                                      border: Border.all(color: Colors.white),
+                                    ),
                                   ),
                                 ),
                               ),
@@ -94,15 +97,23 @@ class _CustomRouteGridScreenState extends State<CustomRouteGridScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          List<Offset> selectedHolds = [];
+          List<int> selectedHolds = [];
           for (int i = 0; i < gridState.length; i++) {
             for (int j = 0; j < gridState[i].length; j++) {
               if (gridState[i][j]) {
-                selectedHolds.add(holdCoordinates[i][j]);
+                int index;
+                if (i % 2 == 0) {
+                  // left to right if it is even row
+                  index = (8 - i) * gridState[i].length + j;
+                } else {
+                  // right to left if it is odd row
+                  index = (8 - i) * gridState[i].length +
+                      (gridState[i].length - 1 - j);
+                }
+                selectedHolds.add(index);
               }
             }
           }
-          // print holds list
           print('Selected Holds: $selectedHolds');
         },
         backgroundColor: Colors.red[400],
