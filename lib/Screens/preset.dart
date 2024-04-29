e01ad19aa28bb991563c22e26ee83729b2183811
Justin Zheng <justinpzheng@gmail.com>
2024-04-29 02:04:41 -0400

    Preset screen with preview and routecompleted button

--- lib/Screens/preset.dart
+++ lib/Screens/preset.dart
@@ -7,58 +7,40 @@ class PresetRoute {
   final String id;
   final String name;
   final String difficulty;
+  final List<int> holds; // Add holds list here
 
   PresetRoute({
     required this.id,
     required this.name,
     required this.difficulty,
+    required this.holds, // Update constructor to include holds
   });
 }
 
 class PresetScreen extends StatelessWidget {
   PresetScreen({super.key});
 
-  // key to access Scaffold state (for side menu navigation)
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    /*List<ListItem> listItems = [
-      ListItem(
-        title: 'Beginner',
-        selectedOption: 'Option 1',
-        dropdownOptions: ['Option 1', 'Option 2', 'Option 3'],
-      ),
-      ListItem(
-        title: 'Intermediate',
-        selectedOption: 'Option 1',
-        dropdownOptions: ['Option 1', 'Option 2', 'Option 3'],
-      ),
-      ListItem(
-          title: 'Expert',
-          selectedOption: 'Option 1',
-          dropdownOptions: ['Option 1', 'Option 2', 'Option 3'])
-    ];*/
-
-    //return MaterialApp(
-      return Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(
-          title: const Text(
-            'PRESET ROUTES',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.red[400],
-          leading: IconButton(
-            icon: const Icon(Icons.menu, color: Colors.white),
-            onPressed: () {
-              // open drawer using key
-              _scaffoldKey.currentState?.openDrawer();
-            },
-          ),
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text(
+          'PRESET ROUTES',
+          style: TextStyle(color: Colors.white),
         ),
-        body: FutureBuilder<List<PresetRoute>>(
-        future: fetchPresetRoutes(),
+        backgroundColor: Colors.red[400],
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.white),
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+        ),
+      ),
+      body: FutureBuilder<List<DocumentSnapshot>>(
+        future: fetchPresetDocuments(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
@@ -67,101 +49,76 @@ class PresetScreen extends StatelessWidget {
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return Center(child: Text('No preset routes available.'));
           } else {
-            return GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
+            return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
-                final presetRoute = snapshot.data![index];
-                return GestureDetector(
-                  onTap: () {
-                    // Navigate to preview screen when tapped
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PresetDisplayScreen(route: presetRoute),
-                      ),
-                    );
-                  },
-                  child: GridTile(
-                    child: Container(
-                      color: Colors.blue, // Example color
-                      child: Center(
-                        child: Text(presetRoute.name),
-                      ),
-        //drawer: const sideMenu(),
-        //body: ListView.builder(
-          //itemCount: listItems.length,
-          //itemBuilder: (BuildContext context, int index) {
-            //return DropdownListItem(listItem: listItems[index]);
-          //},
-        ),
-      ),
-    );
-  },
-);
+                final document = snapshot.data![index];
+                return DropdownTile(document: document);
+              },
+            );
           }
         },
-        ),
+      ),
       drawer: const sideMenu(),
-      );
+    );
   }
-Future<List<PresetRoute>> fetchPresetRoutes() async {
+
+  Future<List<DocumentSnapshot>> fetchPresetDocuments() async {
     try {
-      // Fetch preset routes from Firestore
-      QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('presets').get();
-
-      // Convert Firestore data to PresetRoute objects
-      List<PresetRoute> presetRoutes = querySnapshot.docs.map((doc) {
-        return PresetRoute(
-          id: doc.id,
-          name: doc['name'],
-          difficulty: doc['difficulty'],
-        );
-      }).toList();
-
-      return presetRoutes;
+      QuerySnapshot querySnapshot =
+          await FirebaseFirestore.instance.collection('presets').get();
+      return querySnapshot.docs;
     } catch (e) {
-      print('Error fetching preset routes: $e');
-      return []; // Return empty list if error occurs
+      print('Error fetching preset documents: $e');
+      return [];
     }
   }
 }
 
+class DropdownTile extends StatelessWidget {
+  final DocumentSnapshot document;
 
+  const DropdownTile({Key? key, required this.document}) : super(key: key);
 
-
-
-/*class DropdownListItem extends StatefulWidget {
-  final ListItem listItem;
-
-  const DropdownListItem({super.key, required this.listItem});
-
-  @override
-  _DropdownListItemState createState() => _DropdownListItemState();
-}
-
-class _DropdownListItemState extends State<DropdownListItem> {
   @override
   Widget build(BuildContext context) {
     return ExpansionTile(
       title: Text(
-        widget.listItem.title,
+        document.id.toUpperCase(),
         style: TextStyle(fontWeight: FontWeight.w600, color: Colors.red[400]),
       ),
-      children: widget.listItem.dropdownOptions.map((option) {
-        return ListTile(
-          title: Text(option),
-          onTap: () {
-            setState(() {
-              widget.listItem.selectedOption = option;
-            });
+      children: [
+        ...List.generate(
+          3,
+          (index) {
+            final fieldName = '${document.id}${index + 1}';
+            final holdsArray = List<int>.from(document[fieldName] ?? []);
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PresetDisplayScreen(
+                          route: PresetRoute(
+                            id: '$index',
+                            name: fieldName,
+                            difficulty: document.id,
+                            holds: holdsArray, // Populate holds with the array
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  child: Text(fieldName),
+                ),
+              ],
+            );
           },
-        );
-      }).toList(),
+        ),
+      ],
     );
   }
-}*/
+}
--- lib/Screens/preset_display.dart
+++ lib/Screens/preset_display.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Import FirebaseAuth
 import 'package:mobileapp/Screens/preset.dart';
 
-class PresetDisplayScreen extends StatelessWidget {
+class PresetDisplayScreen extends StatefulWidget {
   final PresetRoute route;
 
   const PresetDisplayScreen({Key? key, required this.route}) : super(key: key);
+
+  @override
+  _PresetDisplayScreenState createState() => _PresetDisplayScreenState();
+}
+
+class _PresetDisplayScreenState extends State<PresetDisplayScreen> {
+  List<List<bool>> gridState = List.generate(9, (_) => List.filled(5, false));
+
+  @override
+  void initState() {
+    super.initState();
+    _updateGridState(
+        widget.route.holds); // Update grid state with holds from route
+  }
+
+  void _updateGridState(List<int> holds) {
+    // Reset grid state
+    for (int i = 0; i < gridState.length; i++) {
+      for (int j = 0; j < gridState[i].length; j++) {
+        gridState[i][j] = false;
+      }
+    }
+
+    // Set holds in grid state
+    for (int holdIndex in holds) {
+      int rowIndex = 8 - (holdIndex ~/ 5);
+      int colIndex = holdIndex % 5;
+      gridState[rowIndex][colIndex] = true;
+    }
+    setState(() {});
+  }
+
+  Future<void> _updateCompletedRoutes() async {
+    try {
+      // Get current user's ID using FirebaseAuth
+      final currentUser = FirebaseAuth.instance.currentUser;
+      if (currentUser != null) {
+        final currentUserID = currentUser.uid;
+
+        // Retrieve user document from Firestore
+        final userDoc =
+            FirebaseFirestore.instance.collection('users').doc(currentUserID);
+
+        // Check if completedRoutes field exists
+        final userSnapshot = await userDoc.get();
+        int completedRoutes = 1;
+        if (userSnapshot.exists) {
+          final userData = userSnapshot.data() as Map<String, dynamic>?;
+          if (userData != null && userData.containsKey('completedRoutes')) {
+            completedRoutes = userData['completedRoutes'] + 1;
+          }
+        }
+
+        // Update user document with completedRoutes field
+        await userDoc
+            .set({'completedRoutes': completedRoutes}, SetOptions(merge: true));
+      } else {
+        print('User not authenticated.');
+      }
+    } catch (error) {
+      print('Error updating completedRoutes: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,22 +83,89 @@ class PresetDisplayScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Route Name: ${route.name}',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        child: _buildGridSystem(),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SizedBox(
+            width: 40,
+            child: FloatingActionButton(
+              onPressed: () {
+                // Add your action for the left button
+                _updateCompletedRoutes();
+              },
+              mini: true,
+              child: Icon(Icons.check),
             ),
-            SizedBox(height: 10),
-            Text(
-              'Difficulty: ${route.difficulty}',
-              style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(width: 16), // Add some space between the buttons
+          SizedBox(
+            width: 40,
+            child: FloatingActionButton(
+              onPressed: () {
+                // Add your action for the right button
+              },
+              mini: true,
+              child: Icon(Icons.arrow_forward),
             ),
-            SizedBox(height: 20),
-            // Add more details about the route as needed
-          ],
-        ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGridSystem() {
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width * 1.8, // image width
+      height: size.width * 1.8, // image height
+      child: Stack(
+        children: [
+          Image.asset(
+            'assets/climbing_board.jpeg', // Change this to the appropriate image for preset routes
+            fit: BoxFit.cover,
+          ),
+          Positioned.fill(
+            child: Transform.translate(
+              offset: Offset(3.0, 28.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: List.generate(
+                  gridState.length,
+                  (rowIndex) => Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      gridState[rowIndex].length,
+                      (colIndex) => GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            gridState[rowIndex][colIndex] =
+                                !gridState[rowIndex][colIndex];
+                          });
+                        },
+                        child: Container(
+                          width: size.width * 0.09,
+                          height: size.width * 0.09,
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 22.6,
+                            vertical: 11.5,
+                          ),
+                          decoration: BoxDecoration(
+                            color: gridState[rowIndex][colIndex]
+                                ? Colors.blue
+                                : Colors.transparent,
+                            border: Border.all(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
